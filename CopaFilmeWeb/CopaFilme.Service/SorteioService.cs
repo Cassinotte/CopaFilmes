1efccea417f6de45db9cd1a7aa79012d9a0c011e
Eduardo Cassinotte <ea.cassinotte@hotmail.com>
2020-03-31 02:07:28 -0300

    Calculo Algoritomo

--- CopaFilmeWeb/CopaFilme.Service/SorteioService.cs
+++ CopaFilmeWeb/CopaFilme.Service/SorteioService.cs
@@ -1,5 +1,6 @@
 ﻿using CopaFilme.Integration;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -34,16 +35,30 @@ namespace CopaFilme.Service
 
             // Ordenar os filmes alfabeticamente
             var selecao = retorno.Where(x => idsFilmes.Contains(x.id))
-                    .OrderBy(x => x.titulo);
-
-            var metade = (int)idsFilmes.Length / 2;
+                    .OrderBy(x => x.titulo).ToList();
 
             // Fazer com que os filmes disputem em eliminatórias da seguinte forma: o filme na primeira
             // posição disputará contra o da última posição, o segundo com o penúltimo
-            var selecaoPrincipal = retorno.Take(metade);
+            var selecaoPrincipal = selecao.Select((x, i) => new { elemt = x, index = i })
+                .Select(x => new KeyValuePair<FilmesResponse, FilmesResponse>(x.elemt,selecao[idsFilmes.Length - x.index]))
+                .Take((int)idsFilmes.Length / 2);
 
-            var x = from sp in selecaoPrincipal
-                    let elementoPar = retorno.IndexOf(sp).
+            // Fase de Eliminatórias
+
+        }
+
+        private (FilmesResponse, List<KeyValuePair<FilmesResponse, FilmesResponse>> filme) CalcularVencedor(List<KeyValuePair<FilmesResponse, FilmesResponse>> filme)
+        {
+            var vencedor = filme.FirstOrDefault();
+            filme.Remove(vencedor);
+
+            if (vencedor.Key.nota > vencedor.Value.nota || vencedor.Key.titulo.CompareTo(vencedor.Value.titulo) > 0)
+            {
+                return (vencedor.Key, filme);
+            }
+
+            return (vencedor.Value, filme);
+
 
         }
 
