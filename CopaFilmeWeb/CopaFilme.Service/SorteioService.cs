29a095605e3a2c639244d9ff5a900e956a0082b5
Eduardo Cassinotte <ea.cassinotte@hotmail.com>
2020-03-31 01:33:38 -0300

    Inclusão teste unitario

--- CopaFilmeWeb/CopaFilme.Service/SorteioService.cs
+++ CopaFilmeWeb/CopaFilme.Service/SorteioService.cs
@@ -24,15 +24,26 @@ namespace CopaFilme.Service
 
         public async Task GetVencedorCopaFilme(string[] idsFilmes, CancellationToken ct )
         {
+
+            if ( idsFilmes.Length % 2 != 0)
+            {
+                throw new FormatException();
+            }
+
             var retorno = await CopaFilmeBase.GetFilmesAsync(ct);
 
             // Ordenar os filmes alfabeticamente
             var selecao = retorno.Where(x => idsFilmes.Contains(x.id))
                     .OrderBy(x => x.titulo);
 
+            var metade = (int)idsFilmes.Length / 2;
+
             // Fazer com que os filmes disputem em eliminatórias da seguinte forma: o filme na primeira
             // posição disputará contra o da última posição, o segundo com o penúltimo
+            var selecaoPrincipal = retorno.Take(metade);
 
+            var x = from sp in selecaoPrincipal
+                    let elementoPar = retorno.IndexOf(sp).
 
         }
 
